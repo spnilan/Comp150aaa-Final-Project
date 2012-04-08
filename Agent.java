9d091e143dd12a5143dca5e6232d967ba74548fc
Constantin Berzan <cberza01@lab120e.(none)>
2012-04-08 19:46:17 -0400

    fix force-based motion and refactor Agent.step() function

--- Agent.java
+++ Agent.java
@@ -16,10 +16,11 @@ public class Agent implements Steppable
     protected static final double eatingRange = 5;
     protected static final int stepsSatiated = 10;
 
+    protected static final double foodFactor = 3.0;
     protected static final double defaultFlockingFactor = 1.0;
     protected static double flockingFactor;
-    protected static double repulsionFactor = 1.0;
-    protected static double foodFactor = 3;
+    protected static final double repulsionFactor = 1.0;
+    protected static final double randomnessFactor = 0.2;
 
     // Agent data:
     public int id;
@@ -50,11 +51,6 @@ public class Agent implements Steppable
     {
         DiseaseSpread sim = (DiseaseSpread)state;
 
-        // Check if the agent has recovered from the disease with a
-        {
-            infected = false;
-        }
-
         // Drain energy and remove agent from environment & schedule if the
         // energy drops to zero.
         double drain = energyDrainPerStep;
@@ -69,103 +65,162 @@ public class Agent implements Steppable
             return;
         }
 
-        // Go through the list of nearby items and do something depending on
-        // what type they are.
+        // Collect nearby items and sort them by type.
         Bag neighbors = sim.environment.getObjectsWithinDistance(location, sensoryRange);
-        Food bestItem = null;
-        MutableDouble2D sumForces = new MutableDouble2D();
-
         ArrayList<Agent> nearbyAgents = new ArrayList<Agent>();
         ArrayList<Food> nearbyFood = new ArrayList<Food>();
-
         for(int i = 0; i < neighbors.numObjs; i++) {
+            // Ignore ourselves.
+            if(neighbors.objs[i] == this) {
+                continue;
+            }
             // Doc says getObjectsWithinDistance() may return objects outside the
             // range, so we have to ignore those objects.
-            if(sim.environment.getObjectLocation(neighbors.objs[i]).distance(location) > sensoryRange
-                    || neighbors.objs[i] == this) {
+            Double2D objLoc = sim.environment.getObjectLocation(neighbors.objs[i]); 
+            if(objLoc.distance(this.location) > sensoryRange) {
                 continue;
             }
             if(neighbors.objs[i] instanceof Food) {
-                Food item = (Food)neighbors.objs[i];
-                if(bestItem == null || item.energy > bestItem.energy) {
-                    bestItem = item;
-                }
-                nearbyFood.add(item);
+                nearbyFood.add((Food)neighbors.objs[i]);
             } else if(neighbors.objs[i] instanceof Agent) {
-                Agent agent = (Agent)neighbors.objs[i];
-
-                // infect any nearby agents
-                if (infected && location.distance(agent.location) <= infectionRange
-                        && !agent.infected && sim.random.nextDouble() <= sim.disease.probTransmission) {
-                    agent.infected = true;
-                }
-                nearbyAgents.add(agent);
+                nearbyAgents.add((Agent)neighbors.objs[i]);
             }
-
         }
 
-        for (Food item : nearbyFood) {
-          if (item != null) {
-            double d = sim.environment.getObjectLocation(item).distance(location);
-            MutableDouble2D force = new MutableDouble2D();
-            double factor = foodFactor / (d*d);
-            force.setTo( (sim.environment.getObjectLocation(item).x - location.x) * factor, (sim.environment.getObjectLocation(item).y - location.y) * factor);
-            sumForces.addIn(force);
-          }
+        // Take appropriate actions. These are factored out into different
+        // functions for readability.
+        stepEat(state, nearbyFood);
+        stepUpdateInfected(state, nearbyAgents);
+        stepMove(state, nearbyFood, nearbyAgents);
+    }
+
+    /**
+     * Go through the list of nearby food and eat the best item we can find.
+     * The eaten food item gets removed from the environment and the schedule.
+     */
+    private void stepEat(final SimState state, ArrayList<Food> nearbyFood)
+    {
+        DiseaseSpread sim = (DiseaseSpread)state;
+
+        // We can't eat if we are satiated.
+        if(isSatiated()) {
+            stepsUntilCanEat--;
+            return;
         }
-        // If we can eat and we saw food, eat it.
-        if(bestItem != null && bestItem.energy > 0 && !isSatiated() && sim.environment.getObjectLocation(bestItem).distance(location) <= eatingRange) {
+
+        // Find the item with the most energy that is close enough to eat.
+        Food bestItem = null;
+        for(Food item: nearbyFood) {
+            Double2D itemLoc = sim.environment.getObjectLocation(item);
+            if(itemLoc.distance(this.location) > eatingRange) {
+                continue;
+            }
+            if(bestItem == null || item.energy > bestItem.energy) {
+                bestItem = item;
+            }
+        }
+
+        // Eat the best found item, if any.
+        if(bestItem != null) {
             energy += bestItem.energy;
             bestItem.energy = 0;
             sim.environment.remove(bestItem);
             // bestItem will be removed from schedule on its next step().
             System.out.println("Agent " + id + " ate");
             stepsUntilCanEat = stepsSatiated;
-        } else if(isSatiated()) {
-            stepsUntilCanEat--;
         }
+    }
 
-        // Move slightly in a random direction.
-        // TODO attraction / repulsion; flocking / no flocking cases...
+    /**
+     * If infected, recovers from disease with some probability. If healthy,
+     * gets infected with some probability if there are nearby infected agents.
+     */
+    private void stepUpdateInfected(final SimState state,
+                                    ArrayList<Agent> nearbyAgents)
+    {
+        DiseaseSpread sim = (DiseaseSpread)state;
 
-        for (Agent agent : nearbyAgents) {
-            double d = location.distance(agent.location);
-            if (d == 0) {
+        if(infected) {
+            // Recover with some probability.
+            if(sim.random.nextDouble() <= sim.disease.probRecovery) {
+                infected = false;
+                System.out.println("Agent " + id + " recovered");
+            }
+        } else {
+            // Figure out if there is an infected agent nearby.
+            boolean foundInfected = false;
+            for(Agent agent: nearbyAgents) {
+                if(agent.infected &&
+                   agent.location.distance(this.location) <= infectionRange) {
+                    foundInfected = true;
+                    break;
+                }
+            }
+            // If there is an infected agent nearby, get infected with some probability.
+            if(foundInfected && sim.random.nextDouble() <= sim.disease.probTransmission) {
+                infected = true;
+                System.out.println("Agent " + id + " got infected");
+            }
+        }
+    }
+
+    /**
+     * Moves in an appropriate direction based on what food items and other
+     * agents are nearby. This takes into account infection and flocking
+     * parameters.
+     */
+    private void stepMove(final SimState state,
+                          ArrayList<Food> nearbyFood,
+                          ArrayList<Agent> nearbyAgents)
+    {
+        DiseaseSpread sim = (DiseaseSpread)state;
+        MutableDouble2D sumForces = new MutableDouble2D();
+
+        // We are attracted to food.
+        for (Food item : nearbyFood) {
+            Double2D itemLoc = sim.environment.getObjectLocation(item);
+            if(itemLoc == null) {  // this may be null if we just ate the item
                 continue;
             }
-
-            MutableDouble2D force = new MutableDouble2D();
-            if (!infected && agent.infected) {
-               double factor = repulsionFactor / (d*d);
-               force.setTo( (location.x - agent.location.x) * factor, (location.y - agent.location.y) * factor);
-            } else if (!agent.infected) {
-               double factor = flockingFactor / (d*d);
-               force.setTo( (agent.location.x - location.x) * factor, (agent.location.y - location.y) * factor);
-            }
+            double d = itemLoc.distance(this.location);
+            Double2D force = itemLoc.subtract(this.location).multiply(foodFactor / (d * d));
+            // TODO take into account energy of food?
             sumForces.addIn(force);
         }
 
-
-        //System.out.println("Number of nearby agents: " + nearbyAgents.size());
-        //System.out.println("Agent " + id + " force: [x=" + sumForces.x + ", y=" + sumForces.y + "]");
-
-        double dx, dy;
-        if (sumForces.length() > 0) {
-           dx = sumForces.normalize().x;
-           dy = sumForces.normalize().y;
-
-           // throw in some randomness too
-           dx += 0.2 * sim.random.nextDouble();
-           dy += 0.2 * sim.random.nextDouble();
-
-        } else {
-            // move in a random direction
-            dx = 1.5 - 3* sim.random.nextDouble();
-            dy = 1.5 - 3 * sim.random.nextDouble();
+        // We are attracted to healthy agents and repelled by infected agents.
+        // TODO figure out if we need more specific cases, e.g. if (this) is a
+        // health agent or an infected agent.
+        // FIXME this doesn't work very well; need to replace it with some
+        // better flocking behavior.
+        for (Agent other : nearbyAgents) {
+            double d = other.location.distance(this.location);
+            double factor = flockingFactor / (d * d);
+            if(other.infected) {
+                factor = -repulsionFactor / (d * d);
+            }
+            Double2D force = other.location.subtract(this.location).multiply(factor);
+            sumForces.addIn(force);
         }
 
-        location = new Double2D(location.x + dx, location.y + dy);
-        sim.environment.setObjectLocation(this, location);
+        // Add some randomness.
+        {
+            MutableDouble2D force = new MutableDouble2D(sim.random.nextDouble(),
+                                                        sim.random.nextDouble());
+            force.normalize();
+            force.multiplyIn(randomnessFactor);
+            sumForces.normalize();  // otherwise scales are meaningless
+            sumForces.addIn(force);
+        }
 
+        // Move to the location given by the sum of forces.
+        // Careful, need to setObjectLocation and *also* update this.location.
+        if(sumForces.length() > 0) {
+            sumForces.normalize();
+        }
+        sumForces.addIn(this.location);
+        System.out.println("current location: " + this.location + " new location: " + sumForces);
+        this.location = new Double2D(sumForces);
+        sim.environment.setObjectLocation(this, this.location);
     }
 }
