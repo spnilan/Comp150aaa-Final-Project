2f87c567d826149684ab1096e888568d451f2d7d
Constantin Berzan <cberzan@gmail.com>
2012-04-21 11:28:03 -0400

    change conservation of energy (again) to avoid starvation when healthy

--- FoodMaker.java
+++ FoodMaker.java
@@ -3,28 +3,27 @@ import sim.util.Double2D;
 
 /**
  * An "agent" that creates food and adds it to the environment. The FoodMaker
- * tries to achieve conservation of energy. It always tries to maintain
- * numAgentsAlive * targetEnergyPerAgent total energy in the simulation.
+ * tries to achieve conservation of energy.
  */
 public class FoodMaker implements Steppable
 {
     // Food-maker parameters:
-    protected static final double targetEnergyPerAgent = Agent.initialEnergy * 1.5;
+    protected static final double foodEnergyPerAgent = Agent.initialEnergy * 1.5;
     protected static final double energyDeficitThreshold = 500;
     protected static final int spawnCount = 15;
-    protected static final double clusterVariance = 3;
+    protected static final double clusterVariance = 1;
 
     /**
-     * If the totalEnergy of the simulation is smaller than numAgentsAlive *
-     * targetEnergyPerAgent by more than energyDeficitThreshold, creates
+     * If the totalEnergyFood of the simulation is smaller than numAgentsInitial
+     * * foodEnergyPerAgent by more than energyDeficitThreshold, creates
      * spawnCount food items and adds them to the environment and the schedule.
      */
     public void step(final SimState state)
     {
         DiseaseSpread sim = (DiseaseSpread)state;
 
-        double totalEnergyTarget = sim.getAgentsAlive() * targetEnergyPerAgent;
-        if(totalEnergyTarget - sim.totalEnergy > energyDeficitThreshold) {
+        double foodEnergyTarget = sim.numAgentsInitial * foodEnergyPerAgent;
+        if(foodEnergyTarget - sim.getTotalEnergyFood() > energyDeficitThreshold) {
             // Spawn new food items in a Gaussian cluster.
             double cx = sim.random.nextDouble() * sim.xMax,
                    cy = sim.random.nextDouble() * sim.yMax;
