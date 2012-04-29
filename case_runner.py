d337c5018597ff7936893aaf9d3d4cce5a5cb7c6
Constantin Berzan <cberzan@gmail.com>
2012-04-29 17:15:39 -0400

    adjust case_runner params

--- case_runner.py
+++ case_runner.py
@@ -5,10 +5,10 @@ from batch_runner import batch_run, plot_num_agents
 
 if __name__ == "__main__":
     num_trials = 30
-    num_steps = 300
+    num_steps = 800
     for disease in ['none', 'cold', 'malaria', 'avian-flu']:
         for flocking, flocking_factor in [('yes', 2.5), ('no', 0.0)]:
-            for observability, observability_factor in [('good', 0.8), ('poor', 0.2)]:
+            for observability, observability_factor in [('perfect', 1.0), ('poor', 0.2)]:
                 for paranoid, symptom_tolerance in [('yes', 0.2), ('no', 0.8)]:
                     sim_args = ("-for %d -disease %s -flocking %f -observability %f -symptom-tolerance %f" % (
                         num_steps, disease, flocking_factor, observability_factor, symptom_tolerance)).split()
